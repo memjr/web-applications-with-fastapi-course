daaa72a41183b2eb877337785f5d2a7e334aa76a
memjr <memjr73@gmail.com>
2022-06-07 16:58:37 -0400

    End of ch4/9

--- code/ch4-templates/main.py
+++ code/ch4-templates/main.py
@@ -7,9 +7,11 @@ app = fastapi.FastAPI()
 
 @app.get("/")
 def index():
-    return {
-        "message": "Hello world"
-    }
+    content = """
+    <h1>Hello FastAPI Web</h1>
+    <div>This is hwere our fake pypi app will live!</div>
+    """
+    return fastapi.responses.HTMLResponse(content)
 
 
 if __name__ == "__main__":
